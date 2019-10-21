c0c25db8520a8f57a040060daf8797e0471f7bce
Roqueton28 <jose.roque75@gmail.com>
2019-10-20 23:25:17 -0400

    iii. Realiza un conversor de monedas. La cantidad en pesetas que se quiere convertir y la tasa de cambio deberán estar almacenada en variables. Este deberá de realizarlo una función, es decir, la función recibirá las variables necesarias para realizar el cambio.

---
+++ convert.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Convert</title>
+
+</head>
+
+<body>
+
+<p>Conversor de divisas</p>
+
+<?php
+
+$res;
+$can= 100;
+$tasa= 52;
+$res = $can * $tasa;
+	echo "<p>La tasa del dia es $tasa por 1 US dolar</p>";
+	echo "<p>Convertir US $can Dolares a RD pesos = $res </p>";
+	
+?>
+
+</body>
+
+</html>
