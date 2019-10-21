7587861a0eb97bc6df7ee55b149d5bd44797cd84
Roqueton28 <jose.roque75@gmail.com>
2019-10-20 23:01:53 -0400

    i. Escribe un programa que utilice las variables $x y $y. Asígnales los valores 144 y 999 respectivamente. A continuación, muestra por pantalla el valor de cada variable, la suma, la resta, la división y la multiplicación.

---
+++ variables.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title>Variables</title>
+
+</head>
+
+<body>
+
+<p>Cálculo de Variables</p>
+
+<?php
+
+	$n="Jose Roque";
+	$x= 144;
+	$y= 999;
+    $s=0; $s=$x+$y;
+	$r=0; $r=$x-$y;
+	$m=0; $m=$x*$y;
+	$d=0; $d=$x/$y;
+
+  echo "<p>El valor de las variables es:</p>\n";
+  echo "<p>	x = $x </p>\n";
+  echo "<p>	y = $y </p>\n";
+  echo "<p> La suma = $s </p>\n";
+  echo "<p> La resta = $r </p>\n";
+  echo "<p> La multiplicación = $m </p>\n";
+  echo "<p> La división = $d </p>\n";
+  echo "<p> Mi nombre es $n </p>\n";
+  
+?>
+
+</body>
+
+</html>
